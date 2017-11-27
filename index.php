6ea027173eebafdc4608d5df3c10ef3a211d9a18
Dxg125 <Dxg125@ISO-ROBL>
2017-11-27 16:42:50 +0100

    w3 and jquery

--- index.php
+++ index.php
@@ -4,7 +4,19 @@
   Das mitem getInfo chömmer au andersch mache kp
  */
 
-include_once 'Model/egg.class.php';
+include_once 'model/egg.class.php';
+
+echo "<!DOCTYPE html>";
+echo "<html>";
+echo "<head>";
+echo "<title>Title of the document</title>";
+
+echo "<script src='http://code.jquery.com/jquery-latest.min.js'></script>";
+
+echo "<link rel='stylesheet' href='https://www.w3schools.com/w3css/4/w3.css'>";
+
+echo "</head>";
+echo "<body>";
 
 $egg = new egg(color::WHITE, type::MARZIPAN, 15);
 
@@ -13,3 +25,6 @@ echo "<br>";
 echo $egg->getInfo()["color"];
 echo "<br>";
 echo $egg->getInfo()["weight"];
+
+echo "</body>";
+echo "</html>";
