0f55ece897f5993b41f4563d4d5fb1a10f1cf3e0
Dxg125 <Dxg125@ISO-ROBL>
2017-11-27 16:25:52 +0100

    new egg better than before

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <?php
 
-/* 
-    Das mitem getInfo chömmer au andersch mache kp
+/*
+  Das mitem getInfo chömmer au andersch mache kp
  */
 
-include_once 'Controller/egg.php';
+include_once 'Model/egg.class.php';
 
-$egg = new egg;
-
-$egg->setColor("Bananeblau");
-$egg->setType("Gargerspan");
+$egg = new egg(color::WHITE, type::MARZIPAN, 15);
 
 echo $egg->getInfo()["type"];
 echo "<br>";
 echo $egg->getInfo()["color"];
+echo "<br>";
+echo $egg->getInfo()["weight"];
