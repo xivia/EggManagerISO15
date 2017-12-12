265c1988c8db05eb9407abba2b12739b6d690198
Sven Fahrni <sven.fahrni@fhnw.ch>
2017-12-12 15:00:54 +0100

    Makroschissimorphid

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { EierListeComponent } from './eier-liste/eier-liste.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EierListeComponent
   ],
   imports: [
     BrowserModule
