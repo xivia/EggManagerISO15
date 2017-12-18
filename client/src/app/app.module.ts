d4e5e5d316d40144b1b4eb85e36802b23ce5a8b0
Tobias Fuchs <fuchs.tobias1999@gmail.com>
2017-12-18 15:53:42 +0100

    meeerge

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { EierListeComponent } from './eier-liste/eier-liste.component';
     EierListeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
