3fbaa4fe2465e9e593ed7abf4ec71cc1c3b4b933
Dxg125 <Dxg125@ISO-ROBL>
2018-01-08 15:42:40 +0100

    api works

--- controller/php/ajaxHandler.php
+++ controller/php/ajaxHandler.php
@@ -8,12 +8,14 @@ $assoc = json_decode(filter_input(INPUT_POST, "array"), true);
 $action = filter_input(INPUT_POST, "action");
 $command = filter_input(INPUT_POST, "command");
 
+$dbName = "sql11214651";
+
 $sql = new SQL();
 
 if ($command == "egg") {
 
     if ($action == "get") {
-        $res = $sql->query("SELECT * FROM egg.egg;");
+        $res = $sql->query("SELECT * FROM $dbName.egg;");
         echo json_encode($res);
     }
 
@@ -23,10 +25,12 @@ if ($command == "egg") {
 
     if ($action == "add") {//TODO sql insert foreach possibility is here too
         $return = Array();
+
         foreach ($assoc as $a) {
+                    var_dump($a);
             $egg = new egg($a["color"], $a["type"], $a["weight"], $a["name"]);
-            $res = $sql->query("INSERT INTO egg.egg (name, color, weight, type) VALUES ('" . $a["name"] . "', '" . $a["color"] . "', '" . $a["weight"] . "', '" . $a["type"] . "');");
-            $res ? ($return["egg"] = $egg->getInfo()) : ($return["egg"] = $res);
+            $res = $sql->query("INSERT INTO $dbName.egg (name, eggColor, eggSize, eggType) VALUES ('" . $a["name"] . "', '" . $a["color"] . "', '" . $a["weight"] . "', '" . $a["type"] . "');");
+            $res ? (array_push($return, $egg->getInfo())) : ("failed");
         }
         echo json_encode($return);       // echo $res ? (json_encode($egg->getInfo())) : (json_encode(false));
     }
